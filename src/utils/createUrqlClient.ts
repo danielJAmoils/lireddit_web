315d10892947030d9ba409060ad39cd40a3cff7d
Daniel Amoils <danieljamoils@gmail.com>
2020-10-17 20:48:04 -0400

    finish posts pagination and fix file name extension

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -42,19 +42,31 @@ export const cursorPagination = (): Resolver => {
             return undefined
         }
         const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`
-        const isItInTheCache = cache.resolveFieldByKey(entityKey, fieldKey)
-        console.log("is it in the cache: ", isItInTheCache)
+        const isItInTheCache = cache.resolve(
+            cache.resolveFieldByKey(entityKey, fieldKey) as string,
+            "posts"
+        )
         info.partial = !isItInTheCache
+        let hasMore = true
         const results: string[] = []
         fieldInfos.forEach((fi) => {
-            const data = cache.resolveFieldByKey(
+            const key = cache.resolveFieldByKey(
                 entityKey,
                 fi.fieldKey
-            ) as string[]
+            ) as string
+            const data = cache.resolve(key, "posts") as string[]
+            const _hasMore = cache.resolve(key, "hasMore")
+            if (!_hasMore) {
+                hasMore = _hasMore as boolean
+            }
             results.push(...data)
         })
 
-        return results
+        return {
+            __typename: "PaginatedPosts",
+            hasMore,
+            posts: results
+        }
 
         //     const visited = new Set()
         //     let result: NullArray<string> = []
@@ -121,6 +133,9 @@ export const createUrqlClient = (ssrExchange: any) => ({
     exchanges: [
         dedupExchange,
         cacheExchange({
+            keys: {
+                PaginatedPosts: () => null
+            },
             resolvers: {
                 Query: {
                     posts: cursorPagination()
